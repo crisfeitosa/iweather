8e6a4c9c35f73166cff6349bb785e170180791b0
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-01-01 14:26:48 -0300

    Renderizando o componente no teste

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,5 +1,10 @@
-
+import { render } from '@testing-library/react-native';
+import { Input } from '@components/Input';
 
 describe('Component: Input', () => {
-
+  it('should  be render without activity indicator', () => {
+    render (
+      <Input />
+    )
+  })
 });
