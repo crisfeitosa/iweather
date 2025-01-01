98a0501f87424d3d069233c811b4bd3272baed53
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-01-01 15:25:07 -0300

    toHaveBeenCalledWith

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -22,6 +22,6 @@ describe('Component: SelectList', () => {
     const selectedCity = screen.getByText(/campo/i);
     fireEvent.press(selectedCity);
 
-    expect(onPress).toHaveBeenCalledTimes(1);
+    expect(onPress).toHaveBeenCalledWith(data[1]);
   });
 });
