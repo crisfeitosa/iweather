38089bb234f41c0521e59de378fd1a87b3092747
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-01-01 22:35:04 -0300

    before e after

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -6,9 +6,7 @@ import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse";
 
 describe("Screen: Dashboard", () => {
-  it('should be show city weather', async () => {
-    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
-
+  beforeAll(async () => {
     const city = {
       id: '1',
       name: 'Rio do Sul, BR',
@@ -17,6 +15,11 @@ describe("Screen: Dashboard", () => {
     };
 
     await saveStorageCity(city);
+  });
+
+  it('should be show city weather', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
+
     render(<Dashboard />);
 
     const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
@@ -24,15 +27,6 @@ describe("Screen: Dashboard", () => {
   });
 
   it('should be show another selected weather city', async () =>{
-    const city = {
-      id: '1',
-      name: 'Rio do Sul, BR',
-      latitude: 123,
-      longitude: 456
-    };
-
-    await saveStorageCity(city);
-
     jest.spyOn(api, 'get')
       .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
       .mockResolvedValueOnce({ data: mockCityAPIResponse })
